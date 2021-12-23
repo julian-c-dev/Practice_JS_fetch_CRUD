37cb65f1cac263f7b99a216b5026641b379119f1
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2021-12-23 20:48:18 +0000

    Added function on & Delete and Edit records.

--- main.js
+++ main.js
@@ -12,10 +12,11 @@ const formArticle = document.querySelector("form");
 const description = document.getElementById("description");
 const price = document.getElementById("price");
 const stock = document.getElementById("stock");
+const create = document.getElementById("btnCreate");
 
 let option = "";
 
-btnCreate.addEventListener("click", () => {
+create.addEventListener("click", () => {
   description.value = "";
   price.value = "";
   stock.value = "";
@@ -23,7 +24,7 @@ btnCreate.addEventListener("click", () => {
   option = "create";
 });
 
-// function show the results
+// * Function show the results
 const show = (articles) => {
   articles.forEach((article) => {
     results += `<tr>
@@ -31,15 +32,98 @@ const show = (articles) => {
                         <td>${article.description}</td>
                         <td>${article.price}</td>
                         <td>${article.stock}</td>
-                        <td class="text-center"><a class="btnEdit btn btn-primary">Edit</a><a class="btnRemove btn btn-danger">Remove</a></td>
+                        <td class="text-center"><a class="btnEdit btn btn-primary mx-1">Edit</a><a class="btnRemove btn btn-danger">Remove</a></td>
                 </tr>
         `;
   });
   container.innerHTML = results;
 };
 
-// show registers
+// * Show records from API
 fetch(url)
   .then((response) => response.json())
   .then((data) => show(data))
   .catch((err) => console.log(err));
+
+// * Function ON (simulated the jQuery one)
+const on = (element, event, selector, handler) => {
+  element.addEventListener(event, (e) => {
+    if (e.target.closest(selector)) {
+      handler(e);
+    }
+  });
+};
+
+// * Delete records
+on(document, "click", ".btnRemove", (e) => {
+  const record = e.target.parentNode.parentNode;
+  const id = record.firstElementChild.innerHTML;
+  alertify.confirm(
+    "This is a confirm dialog.",
+    function () {
+      fetch(url + id, {
+        method: "DELETE",
+      })
+        .then((res) => res.json())
+        .then(() => location.reload());
+    },
+    function () {
+      alertify.error("Cancel");
+    }
+  );
+});
+
+// * Edit records
+let idForm = 0;
+on(document, "click", ".btnEdit", (e) => {
+  console.log("edit");
+  const record = e.target.parentNode.parentNode;
+  idForm = record.children[0].innerHTML; // ! different way to catch the id (look line 59)
+  const descriptionForm = record.children[1].innerHTML;
+  const priceForm = record.children[2].innerHTML;
+  const stockForm = record.children[3].innerHTML;
+  description.value = descriptionForm;
+  price.value = priceForm;
+  stock.value = stockForm;
+  option = "edit";
+  modalArticle.show();
+});
+
+formArticle.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (option == "create") {
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        description: description.value,
+        price: price.value,
+        stock: stock.value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const newArticle = [];
+        newArticle.push(data);
+        location.reload();
+      });
+  }
+  if (option == "edit") {
+    fetch(url + idForm, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        description: description.value,
+        price: price.value,
+        stock: stock.value,
+      }),
+    })
+      .then((response) => response.json())
+      .then((response) => location.reload());
+  }
+  modalArticle.hide();
+});
